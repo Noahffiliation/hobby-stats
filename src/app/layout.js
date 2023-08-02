2eee3ccf99fe8bd77d0e70e3cbbbd607e060f8bf
noahffiliation <nlindsey15@gmail.com>
2023-08-02 12:03:30 -0400

    Add Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Providers } from "./providers";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: 'Hobby Stats'
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
       <body>
 				<Providers>
 					{children}
+					<Analytics />
 				</Providers>
 			</body>
     </html>
