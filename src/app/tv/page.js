4d70f7f27cc83c55a8d37a715100f8356518803e
noahffiliation <nlindsey15@gmail.com>
2023-08-08 12:24:21 -0400

    Initial work on TV page

--- src/app/tv/page.js
+++ src/app/tv/page.js
@@ -1,11 +1,50 @@
 'use client'
 
+import { useEffect, useState } from "react";
 import Nav from "../components/Nav";
+import axios from "axios";
+
+const TRAKT_HEADER = {
+	'Content-Type': 'application/json',
+	'trakt-api-version': '2',
+	'trakt-api-key': process.env.NEXT_PUBLIC_TRAKT_API_KEY
+};
 
 export default function Home() {
+	const [tv, setTv] = useState([]);
+
+	useEffect(() => {
+		const fetchTv = async () => {
+			axios({
+				method: "GET",
+				url: "https://api.trakt.tv/users/noahffiliation/watchlist/shows/released",
+				headers: TRAKT_HEADER
+			}).then((response) => {
+				response.data = response.data.reverse();
+				setTv(response.data);
+			}).catch((error) => {
+				console.log(error);
+			});
+		};
+
+		fetchTv();
+	}, []);
+
 	return (
 		<div>
-			<Nav></Nav>
+			<Nav />
+
+			<ul>
+				{tv.map((tv) => (
+					<>
+						<li key={tv.id}>
+							{tv.show.title}
+							{tv.show.year ? ` - ${tv.show.year}` : ''}
+						</li>
+						<br />
+					</>
+				))}
+			</ul>
 		</div>
-	)
+	);
 }
